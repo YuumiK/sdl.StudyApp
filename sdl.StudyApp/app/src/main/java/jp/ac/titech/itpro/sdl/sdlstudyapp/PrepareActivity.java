7d6415ba3f1bcf6ffca6bc19f117bc676b2d6f61
Yuumi Konishi <koni.overgang@gmail.com>
2019-06-23 16:37:01 +0900

    add
    Prepare/Review Activities

---
+++ sdl.StudyApp/app/src/main/java/jp/ac/titech/itpro/sdl/sdlstudyapp/PrepareActivity.java
@@ -0,0 +1,12 @@
+package jp.ac.titech.itpro.sdl.sdlstudyapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PrepareActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare);
+    }
+}
